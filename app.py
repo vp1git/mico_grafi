a8346d9ff49c48c4108ff4c163248138f105471b
vp <progar.vasja@gmail.com>
2025-08-24 13:30:04 +0200

    reorganizacija: dodano v expanderje

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ import plotly.io as pio
 from utils import get_data, FILE_ID
 from jinja2 import Template
 
-# %%
 st.set_page_config(layout="wide")
 
 
@@ -24,218 +23,204 @@ if "first_load" not in st.session_state:
     st.cache_data.clear()
 
 
-# %%
 data = get_data_cached()
 figs = {}
 
 
-# %%
 df_hrana = pd.merge(data["log_hrana"], data["vrste_hrana"], how="left", on="vrsta")
 df_hrana["pojedel_kcal"] = df_hrana.apply(
     lambda row: row.pojedel_g * row.kcal_per_g, axis="columns"
 )
 
-
-def get_plot_hrana_vs_vrsta(df, value_col="pojedel_g"):
-    df_pivot = (
-        df.set_index("cas")
-        .groupby("vrsta")
-        .resample("1d")[value_col]
-        .sum()
-        .unstack("vrsta")[df["vrsta"].unique()]
-    )
-    fig = px.area(df_pivot, y=df_pivot.columns[1:])
-    for i in fig["data"]:
-        i["line"]["width"] = 0
-    return fig
-
-
-figs["Hrana (g)"] = get_plot_hrana_vs_vrsta(df_hrana, "pojedel_g")
-figs["Hrana (kcal)"] = get_plot_hrana_vs_vrsta(df_hrana, "pojedel_kcal")
-
-
 # %%
-def get_scatterplot_with_trendline(df, value_col, agg_func, color="blue"):
-    s = df.set_index("cas").resample("1d")[value_col].aggregate(agg_func)
-    fig = px.scatter(
-        x=s.index,
-        y=s,
-        trendline="rolling",
-        trendline_options=dict(window="7d"),
-        color_discrete_sequence=[color],
-    )
-    fig.update_traces(
-        marker={"size": 4, "opacity": 0.9},
-        line={"width": 1.5},
-    )
-    return fig
+with st.expander("Kumulativno procenti dnevnih kalorij", expanded=True):
 
-
-figs["Pojedel (kcal)"] = get_scatterplot_with_trendline(
-    df_hrana, "pojedel_kcal", agg_func="sum", color="green"
-)
-figs["Pojedel (g)"] = get_scatterplot_with_trendline(
-    df_hrana, "pojedel_g", agg_func="sum", color="green"
-)
-figs["Teža (g)"] = get_scatterplot_with_trendline(
-    data["log_teza"], "teza_g", agg_func="mean"
-)
-
-
-# %%
-def get_plot_drugo(df, vrsta, kolicina=False, color="blue"):
-    s = df[df.vrsta.isin([vrsta])].set_index("cas")
-    if kolicina:
-        s = s.resample("1d")["kolicina"].sum()
-    else:
-        s = s.resample("1d")["vrsta"].count()
-    fig = px.bar(
-        x=s.index,
-        y=s,
-        color_discrete_sequence=[color],
-        labels=dict(x="Datum", y=vrsta),
-    )
-    return fig
-
-
-figs["Prednicortone (5 mg tablete)"] = get_plot_drugo(
-    data["log_drugo"],
-    "Prednicortone (prednisolone 5mg tablete) [tablet]",
-    kolicina=True,
-    color="blue",
-)
-figs["Bruhanje"] = get_plot_drugo(
-    data["log_drugo"], "bruhanje", kolicina=False, color="red"
-)
-figs["Driska"] = get_plot_drugo(
-    data["log_drugo"], "driska", kolicina=False, color="orange"
-)
-figs["Mehko kakanje"] = get_plot_drugo(
-    data["log_drugo"], "mehko kakanje", kolicina=False, color="orange"
-)
-figs["Infuzija (mL)"] = get_plot_drugo(
-    data["log_drugo"], "infuzija s.c. (mL)", kolicina=True
-)
-figs["Reglan (10mg tablete)"] = get_plot_drugo(
-    data["log_drugo"], "Reglan (metoclopramide 10 mg tablete) [tablet]", kolicina=True
-)
-figs["Farmatan (tablete)"] = get_plot_drugo(
-    data["log_drugo"], "farmatan (tablete)", kolicina=True
-)
-figs["Prevomax (mL)"] = get_plot_drugo(
-    data["log_drugo"], "Prevomax (maropitant 10 mg/mL) [mL]", kolicina=True
-)
-figs["Mirataz"] = get_plot_drugo(data["log_drugo"], "mirataz (uho)", kolicina=False)
-
-
-# %%
-selected_subplots = st.multiselect(
-    "Izbira grafov",
-    figs.keys(),
-    default=[
-        "Pojedel (kcal)",
-        "Teža (g)",
-        "Prednicortone (5 mg tablete)",
-        "Bruhanje",
-        "Driska",
-    ],
-)
-
-
-# %%
-fig_out = make_subplots(
-    rows=len(selected_subplots),
-    cols=1,
-    shared_xaxes=True,
-    subplot_titles=selected_subplots,
-    vertical_spacing=0.05,
-)
-
-for i, fig_key in enumerate(selected_subplots, start=1):
-    for trace in figs[fig_key].data:
-        fig_out.add_trace(trace, row=i, col=1)
-
-fig_out.update_xaxes(type="date")
-fig_out.update_layout(height=700, width=600)
-
-
-# %%
-st.plotly_chart(fig_out)
-
-st.markdown(f"[Izvorni podatki](https://docs.google.com/spreadsheets/d/{FILE_ID})")
-
-# %%
-st.markdown(f"## Kumulativno procenti dnevnih kalorij:")
-
-
-def get_plot_kcal_kumulativa(df_hrana, date_start, date_end, current_datetime):
-    kcal = df_hrana.set_index("cas")["pojedel_kcal"].loc[
-        date_start : date_end + datetime.timedelta(days=1)
-    ]
-    df = pd.DataFrame(
-        {
-            "dan": kcal.index.date,
-            "ura": kcal.index.time,
-            "kcal": kcal,
-        },
-        index=kcal.index,
-    )
-    df = pd.concat(
-        [
-            df,
-            pd.DataFrame(
-                [
-                    {"dan": d, "ura": datetime.time(0, 0), "kcal": 0}
-                    for d in df["dan"].unique()
-                ]
-            ),
-            pd.DataFrame(
-                [
-                    (
-                        {"dan": d, "ura": current_datetime.time(), "kcal": 0}
-                        if d == current_datetime.date()
-                        else {"dan": d, "ura": datetime.time(23, 59), "kcal": 0}
-                    )
-                    for d in df["dan"].unique()
-                ]
-            ),
+    def get_plot_kcal_kumulativa(df_hrana, date_start, date_end, current_datetime):
+        kcal = df_hrana.set_index("cas")["pojedel_kcal"].loc[
+            date_start : date_end + datetime.timedelta(days=1)
         ]
-    ).sort_values(["dan", "ura"])
+        df = pd.DataFrame(
+            {
+                "dan": kcal.index.date,
+                "ura": kcal.index.time,
+                "kcal": kcal,
+            },
+            index=kcal.index,
+        )
+        df = pd.concat(
+            [
+                df,
+                pd.DataFrame(
+                    [
+                        {"dan": d, "ura": datetime.time(0, 0), "kcal": 0}
+                        for d in df["dan"].unique()
+                    ]
+                ),
+                pd.DataFrame(
+                    [
+                        (
+                            {"dan": d, "ura": current_datetime.time(), "kcal": 0}
+                            if d == current_datetime.date()
+                            else {"dan": d, "ura": datetime.time(23, 59), "kcal": 0}
+                        )
+                        for d in df["dan"].unique()
+                    ]
+                ),
+            ]
+        ).sort_values(["dan", "ura"])
 
-    df["ura"] = df["ura"].map(
-        lambda t: datetime.datetime.combine(current_datetime.date(), t)
+        df["ura"] = df["ura"].map(
+            lambda t: datetime.datetime.combine(current_datetime.date(), t)
+        )
+        df["kcal_kumulativa"] = df.groupby("dan")["kcal"].cumsum()
+        df["kcal_kumulativa_procent"] = df["kcal_kumulativa"] / 225 * 100
+
+        fig = px.line(
+            df,
+            x="ura",
+            y="kcal_kumulativa_procent",
+            color="dan",
+            line_shape="hv",
+            markers=True,
+        )
+
+        for trace in fig.data:
+            trace.opacity = 1.0 if trace.name == str(date_end) else 0.3
+        fig.update_traces(marker=dict(size=5))
+        fig.add_vline(x=current_datetime, line_width=1, line_color="red", opacity=0.5)
+        fig.update_layout(legend={"traceorder": "reversed"})
+
+        return fig
+
+    current_datetime = datetime.datetime.now(tz=ZoneInfo("Europe/Ljubljana"))
+    date_start = datetime.date(2025, 7, 18)
+    date_end = current_datetime.date()
+    fig_kcal_kumulativa = get_plot_kcal_kumulativa(
+        df_hrana,
+        date_start=date_start,
+        date_end=date_end,
+        current_datetime=current_datetime,
     )
-    df["kcal_kumulativa"] = df.groupby("dan")["kcal"].cumsum()
-    df["kcal_kumulativa_procent"] = df["kcal_kumulativa"] / 225 * 100
+    st.plotly_chart(fig_kcal_kumulativa)
 
-    fig = px.line(
-        df,
-        x="ura",
-        y="kcal_kumulativa_procent",
-        color="dan",
-        line_shape="hv",
-        markers=True,
+
+# %%
+with st.expander("Grafi po dnevih"):
+
+    def get_plot_hrana_vs_vrsta(df, value_col="pojedel_g"):
+        df_pivot = (
+            df.set_index("cas")
+            .groupby("vrsta")
+            .resample("1d")[value_col]
+            .sum()
+            .unstack("vrsta")[df["vrsta"].unique()]
+        )
+        fig = px.area(df_pivot, y=df_pivot.columns[1:])
+        for i in fig["data"]:
+            i["line"]["width"] = 0
+        return fig
+
+    figs["Hrana (g)"] = get_plot_hrana_vs_vrsta(df_hrana, "pojedel_g")
+    figs["Hrana (kcal)"] = get_plot_hrana_vs_vrsta(df_hrana, "pojedel_kcal")
+
+    def get_scatterplot_with_trendline(df, value_col, agg_func, color="blue"):
+        s = df.set_index("cas").resample("1d")[value_col].aggregate(agg_func)
+        fig = px.scatter(
+            x=s.index,
+            y=s,
+            trendline="rolling",
+            trendline_options=dict(window="7d"),
+            color_discrete_sequence=[color],
+        )
+        fig.update_traces(
+            marker={"size": 4, "opacity": 0.9},
+            line={"width": 1.5},
+        )
+        return fig
+
+    figs["Pojedel (kcal)"] = get_scatterplot_with_trendline(
+        df_hrana, "pojedel_kcal", agg_func="sum", color="green"
+    )
+    figs["Pojedel (g)"] = get_scatterplot_with_trendline(
+        df_hrana, "pojedel_g", agg_func="sum", color="green"
+    )
+    figs["Teža (g)"] = get_scatterplot_with_trendline(
+        data["log_teza"], "teza_g", agg_func="mean"
     )
 
-    for trace in fig.data:
-        trace.opacity = 1.0 if trace.name == str(date_end) else 0.3
-    fig.update_traces(marker=dict(size=5))
-    fig.add_vline(x=current_datetime, line_width=1, line_color="red", opacity=0.5)
-    fig.update_layout(legend={"traceorder": "reversed"})
+    def get_plot_drugo(df, vrsta, kolicina=False, color="blue"):
+        s = df[df.vrsta.isin([vrsta])].set_index("cas")
+        if kolicina:
+            s = s.resample("1d")["kolicina"].sum()
+        else:
+            s = s.resample("1d")["vrsta"].count()
+        fig = px.bar(
+            x=s.index,
+            y=s,
+            color_discrete_sequence=[color],
+            labels=dict(x="Datum", y=vrsta),
+        )
+        return fig
 
-    return fig
+    figs["Prednicortone (5 mg tablete)"] = get_plot_drugo(
+        data["log_drugo"],
+        "Prednicortone (prednisolone 5mg tablete) [tablet]",
+        kolicina=True,
+        color="blue",
+    )
+    figs["Bruhanje"] = get_plot_drugo(
+        data["log_drugo"], "bruhanje", kolicina=False, color="red"
+    )
+    figs["Driska"] = get_plot_drugo(
+        data["log_drugo"], "driska", kolicina=False, color="orange"
+    )
+    figs["Mehko kakanje"] = get_plot_drugo(
+        data["log_drugo"], "mehko kakanje", kolicina=False, color="orange"
+    )
+    figs["Infuzija (mL)"] = get_plot_drugo(
+        data["log_drugo"], "infuzija s.c. (mL)", kolicina=True
+    )
+    figs["Reglan (10mg tablete)"] = get_plot_drugo(
+        data["log_drugo"],
+        "Reglan (metoclopramide 10 mg tablete) [tablet]",
+        kolicina=True,
+    )
+    figs["Farmatan (tablete)"] = get_plot_drugo(
+        data["log_drugo"], "farmatan (tablete)", kolicina=True
+    )
+    figs["Prevomax (mL)"] = get_plot_drugo(
+        data["log_drugo"], "Prevomax (maropitant 10 mg/mL) [mL]", kolicina=True
+    )
+    figs["Mirataz"] = get_plot_drugo(data["log_drugo"], "mirataz (uho)", kolicina=False)
 
+    selected_subplots = st.multiselect(
+        "Izbira grafov",
+        figs.keys(),
+        default=[
+            "Pojedel (kcal)",
+            "Teža (g)",
+            "Prednicortone (5 mg tablete)",
+            "Bruhanje",
+            "Driska",
+        ],
+    )
 
-current_datetime = datetime.datetime.now(tz=ZoneInfo("Europe/Ljubljana"))
-date_start = datetime.date(2025, 7, 18)
-date_end = current_datetime.date()
-fig_kcal_kumulativa = get_plot_kcal_kumulativa(
-    df_hrana,
-    date_start=date_start,
-    date_end=date_end,
-    current_datetime=current_datetime,
-)
-st.plotly_chart(fig_kcal_kumulativa)
+    fig_out = make_subplots(
+        rows=len(selected_subplots),
+        cols=1,
+        shared_xaxes=True,
+        subplot_titles=selected_subplots,
+        vertical_spacing=0.05,
+    )
+
+    for i, fig_key in enumerate(selected_subplots, start=1):
+        for trace in figs[fig_key].data:
+            fig_out.add_trace(trace, row=i, col=1)
+
+    fig_out.update_xaxes(type="date")
+    fig_out.update_layout(height=700, width=600)
+
+    st.plotly_chart(fig_out)
 
 
 # %%
@@ -278,7 +263,9 @@ Pojedel (kcal): {{item.kcal}} ({{item.kcal_percent}}%)
 
     # Group by day and render
     items = []
-    for date, group in df.groupby(df["dan"]):
+    for date, group in df.sort_values(["dan", "ura"], ascending=[False, True]).groupby(
+        df["dan"], sort=False
+    ):
         group_df = group[["ura", "vrsta", "kolicina"]].reset_index(drop=True).fillna("")
         items.append(
             {
@@ -313,3 +300,4 @@ kategorije = st.multiselect(
 st.markdown(get_summary_for_dates(date_start, date_end, kategorije))
 
 # %%
+st.markdown(f"[Izvorni podatki](https://docs.google.com/spreadsheets/d/{FILE_ID})")
