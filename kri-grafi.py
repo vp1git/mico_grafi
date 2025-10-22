810a3c7bb5b25ddc94ff6c24af089ecbb824130c
vp <progar.vasja@gmail.com>
2025-10-22 20:59:20 +0200

    dodane enote v grafe za kri

--- kri-grafi.py
+++ kri-grafi.py
@@ -4,33 +4,14 @@ import seaborn as sns
 import matplotlib.dates as mdates
 from datetime import datetime
 
-# import matplotlib.pyplot as plt
-# import matplotlib.dates as mdates
-
 # %%
 data = get_data()
-
 df_kri = data["kri"]
-
 df_kri_cols = data["kri_stolpci"]
 
 # %%
-# df_kri["datum"] = df_kri["cas"].dt.date
-# df_kri_melted = df_kri.drop(columns="cas").melt(id_vars="datum")
 df_kri_melted = df_kri.melt(id_vars="cas")
 df_kri_cols = df_kri_cols.set_index("kratica")
-
-# %%
-# plt.figure()
-
-# date_format = mdates.DateFormatter('%Y-%m')
-# plt.gca().xaxis.set_major_formatter(date_format)
-
-# # Rotate date labels for better readability
-# plt.xticks(rotation=45)
-
-# plt.tight_layout() # Adjust layout to prevent labels from overlapping
-# plt.show()
 
 # %%
 sns.set_style("whitegrid")
@@ -43,13 +24,13 @@ for ax, variable in zip(g.axes.flat, df_kri_cols.index):
     ax.axhspan(lower, upper, linewidth=2, color="green", alpha=0.3)
     ax.tick_params(axis="x", labelrotation=90, labelbottom=True)
     ax.xaxis.set_major_formatter(mdates.DateFormatter("%b"))
+    ax.set_title(f'{variable} [{df_kri_cols["merska enota"][variable]}]', size=16)
 
 g.fig.subplots_adjust(hspace=0.5)
-g.set_titles(template="{col_name}", size=16)
 g.set_axis_labels("", "")
 
 # %%
-current_date = datetime.now().strftime('%Y-%m-%d')
+current_date = datetime.now().strftime("%Y-%m-%d")
 g.savefig(f"output/Mico - kri grafi - {current_date}.pdf")
 
 # %%
